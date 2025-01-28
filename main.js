60bcd086e0d45aa7958756e3a16850d2617de6f3
Tyler Harris <you@example.com>
2025-01-27 19:47:28 -0500

    notes basic tes

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+//lesson tabs
+
 function openLesson(lessontypeName) {
   var i;
   var x = document.getElementsByClassName("lessontype");
@@ -7,4 +10,55 @@ function openLesson(lessontypeName) {
   document.getElementById(lessontypeName).style.display = "block";  
 }
 
+//create a note
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
 
+
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+//local storage
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+
+createBtn.addEventListener("click", ()=>{
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "delete_outline.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+//delete a note
+
+notesContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "IMG"){
+    e.target.parentElement.remove();
+    updateStorage();
+}
+else if(e.target.tagName === "P"){
+  notes = document.querySelectorAll(".input-box");
+  notes.forEach(nt => {
+    nt.onkeyup = function(){
+          updateStorage();
+    }
+  })
+}
+})
+
+
+document.addEventListener("keydown", event =>{
+  if(event.key === "Enter"){
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+})
